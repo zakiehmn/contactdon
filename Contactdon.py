4b8e5349e5be9d766df46c5d3a01e4cb633a7456
zakie <zakiehmantashloo@gmail.com>
2021-08-17 19:32:14 +0430

    added class Contactdon and add_Contact function to Contactdon file

--- Contactdon.py
+++ Contactdon.py
@@ -1,1 +1,15 @@
 
+class Contactdon:
+    contactList
+
+    def add_contact(fName,lName,emailAddress,phoneNumber):
+        newContact=Contact()
+        newContact.set_fName(fName)
+        newContact.set_lName(lName)
+        newContact.set_emailAddress(emailAddress)
+        newContact.set_phoneNumber(phoneNumber)
+        newContact.set_ID
+        contactList.append(newContact)
+
+        
+
