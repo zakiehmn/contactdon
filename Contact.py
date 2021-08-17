24d9874f3b11c3e93560486257f85860b7296f44
zakie <zakiehmantashloo@gmail.com>
2021-08-17 13:50:15 +0430

    added constructor setter and getter function

---
+++ Contact.py
@@ -0,0 +1,51 @@
+class Contact:
+    fName
+    lName
+    emailAddress
+    phoneNumber
+    ID=0
+
+    def __init__(self,fName,lName,emailAddress,phoneNumber,ID):
+        self._fName=fName
+        self._lName=lName
+        self._emailAddress=emailAddress
+        self._phoneNumber=phoneNumber
+        set_ID(ID)
+
+    def set_fName(self,fName):
+        self._fName=fName
+
+    def set_lName(self,lName):
+        self._lName=lName
+
+    def set_emailAddress(self,emailAddress):
+        self._emailAddress=emailAddress
+
+    def set_phoneNumber(self,phoneNumber):
+        self._phoneNumber=phoneNumber
+
+    def set_ID(self,ID):
+        ID=ID+1
+        self._ID=ID
+
+    def get_fName(self):
+        return self._fName
+
+    def get_lName(self):
+        return self._lName
+
+    def get_emailAddress(self):
+        return self._emailAddress
+
+    def get_phoneNumber(self):
+        return self._phoneNumber
+
+    def get_ID(self):
+        return self._ID
+
+    
+
+    
+        
+        
+
