e4e0f68af78c05e6ed3844f167555cbfaab03b89
zakie <zakiehmantashloo@gmail.com>
2021-08-23 17:24:54 +0430

    added check_unique function

--- Contactdon.py
+++ Contactdon.py
@@ -29,9 +29,35 @@ class Contactdon:
                     emailaddress=splitCommand[x+1]
                 if(splitCommand[x]=="-p"):
                     phonenumber=splitCommand[x+1]
+            if(self.check_unique(fname, lname, emailaddress, phonenumber)==True):
+                self.add_contact(fname, lname, emailaddress, phonenumber)
+            else:
+                print("command failed")
         else:
             print("command failed")
-        self.add_contact(fname, lname, emailaddress, phonenumber)
+        self.command()
+        
+
+    def check_unique(self,fname,lname,emailaddress,phonenumber):
+        check=False
+        n=len(self.contactList)
+        checkfnln=True
+        for j in range(n):
+            if(self.contactList[j]._fName==fname and self.contactList[j]._lName==lname):
+                    checkfnln=False
+        if(n==0):
+           check=True
+        if(n>0):
+            for i in range(n):
+                if(checkfnln==True and self.contactList[i]._emailAddress!=emailaddress and self.contactList[i]._phoneNumber!=phonenumber):
+                    check=True
+                else:
+                    check=False
+
+        return check
+
+
+
 
     # def printdata(self,fname,lname,emailaddress,phonenumber):
     #     print(fname,lname,emailaddress,phonenumber)
