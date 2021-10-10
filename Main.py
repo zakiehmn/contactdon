eb51c7014c705e9385125cdb44a86005b46289c0
zakie <zakiehmantashloo@gmail.com>
2021-10-10 16:26:50 +0330

    refactor main class

--- Main.py
+++ Main.py
@@ -1,15 +1,12 @@
 from UserInterface import UserInterface
 from Contactdon import Contactdon
 class main:
-    # from UserInterface import UserInterface
-    # from Contactdon import Contactdon
     contactdon = Contactdon()
-    # contactdon.input_command()
-
     userInterface = UserInterface(contactdon)
     userInterface.run()
-    # if __name__ == "__main__":
-    #     main()
+    
+    if __name__ == "__main__":
+        main()
 
     
     
